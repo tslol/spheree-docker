70dae55246697861dbdabefd7b0f4702580e8af8
Tristan Schlarman <tristanjschlarman@gmail.com>
2024-07-29 17:14:11 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "TYPESENSE_API_KEY=${TYPESENSE_API_KEY}"
-typesense-server --data-dir /data --api-key=${TYPESENSE_API_KEY} --enable-cors
+/usr/bin/typesense-server --data-dir /data --api-key=${TYPESENSE_API_KEY} --enable-cors
